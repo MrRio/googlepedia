dc96ca87e054f2be22f16f5a09ae05ec221f31df
James Hall <rio@x5g.com>
2011-04-07 05:04:51 +0100

    New version 0.8

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 APP_NAME=googlepedia    # short-name, jar and xpi files name.
 BUILT_FOR=fx         # Application that the extension is built for, e.g. 'fx' for firefox
-VERSION=0.5.6        # Application version number
+VERSION=0.8	      # Application version number
 
 rm *.xpi
 zip -vr $APP_NAME-$VERSION-$BUILT_FOR.xpi * -x@exclude.lst
